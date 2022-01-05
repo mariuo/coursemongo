9b0302c4ce9190ae6aa428fee209b224de98c3b0
mariuo <mariuo@gmail.com>
2022-01-05 08:18:15 -0500

    GET findById

--- src/main/java/com/devsuperior/coursemongo/service/UserService.java
+++ src/main/java/com/devsuperior/coursemongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.coursemongo.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.coursemongo.models.dto.UserDTO;
 import com.devsuperior.coursemongo.models.entities.User;
 import com.devsuperior.coursemongo.repositories.UserRepository;
+import com.devsuperior.coursemongo.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,5 +22,11 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(()-> new ResourceNotFoundException("Obj não encontrado!"));
+		return new UserDTO(entity);
 	}
 }
