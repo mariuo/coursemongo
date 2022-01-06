b3ef42761acba726a0959e68cb34b59dadfe4c05
mariuo <mariuo@gmail.com>
2022-01-06 12:12:36 -0500

    GET Post fullSearch

--- src/main/java/com/devsuperior/coursemongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/coursemongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.coursemongo.repositories;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,6 +15,27 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{ 'title' : { $regex: ?0, $options: 'i'}}")
 	List<Post> searchTitle(String text);
 	
+	@Query("{ $and: [ { 'moment': {$gte: ?1}}, { 'moment': {$lte: ?2}} , "
+			+ "{ $or: [ {'title' : { $regex: ?0, $options: 'i'}}, {'body' : { $regex: ?0, $options: 'i'}}, {'comments.text' : { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-
+	
 }
+
+// { $and: [ {<expression1> }, { <expression2> } ,... ,{ <expressionN> } ] }
+// { field: { $gte: value } }
+// { field: { $lte: value } }
+
+// {moment: {$gte: ?1}}
+// {moment: {$lte: ?2}}
+// {'title' : { $regex: ?0, $options: 'i'}}
+// {'body' : { $regex: ?0, $options: 'i'}}
+// {'comments.text' : { $regex: ?0, $options: 'i'}}
+
+// { $or: [ { <expression1> }, { <expression2> }, ... , { <expressionN> } ] }
+
+// { $or: [ {'title' : { $regex: ?0, $options: 'i'}}, {'body' : { $regex: ?0, $options: 'i'}}, {'comments.text' : { $regex: ?0, $options: 'i'}} ] }
+
+
+// { $and: [ {moment: {$gte: ?1}, {moment: {$gte: ?2}} , { $or: [ {'title' : { $regex: ?0, $options: 'i'}}, {'body' : { $regex: ?0, $options: 'i'}}, {'comments.text' : { $regex: ?0, $options: 'i'}} ] } ] }
--- src/main/java/com/devsuperior/coursemongo/service/PostService.java
+++ src/main/java/com/devsuperior/coursemongo/service/PostService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.coursemongo.service;
 
+import java.time.DateTimeException;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,8 +32,22 @@ public class PostService {
 	}
 	
 	public List<PostDTO> findByTitle(String text){
-		List<Post> list = repository.searchTitle(text);
-		
+		List<Post> list = repository.searchTitle(text);		
 		return list.stream().map(x-> new PostDTO(x)).collect(Collectors.toList());
+	}
+	public List<PostDTO> fullSearch(String text, String start, String end){
+		Instant startMoment = convertMoment(start, Instant.ofEpochMilli(0L));
+		Instant endMoment = convertMoment(end, Instant.now());
+		List<Post> list = repository.fullSearch(text, startMoment, endMoment);		
+		return list.stream().map(x-> new PostDTO(x)).collect(Collectors.toList());
+	}
+
+	private Instant convertMoment(String originalString, Instant alternative) {
+		try {
+			return Instant.parse(originalString);
+		}
+		catch(DateTimeException e) {
+			return alternative;
+		}
 	}
 }
